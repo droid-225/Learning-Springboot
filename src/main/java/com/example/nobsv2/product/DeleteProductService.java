85587983750ff1083f872882b653c2c016436e15
Rishit Shah <aryanrishit1@gmail.com>
2025-01-22 22:48:43 +0530

    Finished Video 1

---
+++ src/main/java/com/example/nobsv2/product/DeleteProductService.java
@@ -0,0 +1,16 @@
+package com.example.nobsv2.product;
+
+import com.example.nobsv2.Command;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteProductService implements Command<Void, String> {
+
+    @Override
+    public ResponseEntity<String> execute(Void input) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product Deleted");
+    }
+
+}
